d7a0b482fc9e8b568e260010ccbbb7123e111c46
NiranjanPatil <nirnjanpatil5434@gmail.com>
2023-09-11 21:00:50 +0530

    my personal portfolio-v3 navbar fixed commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const nav = document.getElementById('nav')
 const toggleIcon = document.getElementById('toggle-icon')
 const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
+const changeText = document.getElementById('change-text')
 
 //for (dry) image
 function imageMode(color) {
@@ -12,7 +13,6 @@ function imageMode(color) {
 
 //Dark Mode
 function darkMode() {
-    nav.style.backgroundColor = 'rgba(0,0,0,0.5)';
     textBox.style.backgroundColor = 'rgba(255,255,255,0.5)';
     toggleIcon.children[0].textContent = 'Dark Mode';
     toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
@@ -20,7 +20,6 @@ function darkMode() {
 }
 //Light Mode
 function lightMode() {
-    nav.style.backgroundColor = 'rgba(255,255,255,0.5)';
     textBox.style.backgroundColor = 'rgba(0,0,0,0.5)';
     toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
